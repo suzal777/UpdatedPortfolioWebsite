579a7e1147261e23be93e8da619c56cc17a67971
Sujal Phaiju <98390144+suzal777@users.noreply.github.com>
2025-06-02 23:30:20 +0545

    Create viewCounter lambda_function.py

---
+++ lambda/viewCounter/lambda_function.py
@@ -0,0 +1,44 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('PortfolioViewCounter')  # DynamoDB table name
+ITEM_ID = 'page'  # Partition key value
+
+def lambda_handler(event, context):
+    try:
+        response = table.update_item(
+            Key={'id': ITEM_ID},
+            UpdateExpression="SET #c = if_not_exists(#c, :zero) + :inc",
+            ExpressionAttributeNames={"#c": "count"},
+            ExpressionAttributeValues={":inc": 1, ":zero": 0},
+            ReturnValues="UPDATED_NEW"
+        )
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+            },
+            'body': json.dumps({
+                'count': int(response['Attributes']['count'])
+            })
+        }
+
+    except ClientError as e:
+        print("Error:", e)
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+            },
+            'body': json.dumps({
+                'error': 'Internal Server Error',
+                'details': str(e)
+            })
+        }
